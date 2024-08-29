cdce56ba639ed50610cbf98bfeeb3554bf62713a
Victor Cavalcanti <victorcmarques.dev@gmail.com>
2024-08-28 22:28:55 -0300

    adicionando fontes

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { View, Text } from "react-native";
+import AppLoading from "expo-app-loading";
+import {
+    useFonts,
+    Poppins_300Light,
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+    Poppins_800ExtraBold,
+} from "@expo-google-fonts/poppins";
+
+import {DMSans_400Regular} from "@expo-google-fonts/dm-sans";
+import {DMSerifDisplay_400Regular} from "@expo-google-fonts/dm-serif-display"
+
+const App: React.FC = () => {
+    const [fontsLoaded] = useFonts({
+        Poppins_300Light,
+        Poppins_400Regular,
+        Poppins_500Medium,
+        Poppins_700Bold,
+        Poppins_800ExtraBold,
+        DMSans_400Regular,
+        DMSerifDisplay_400Regular,
+    })
+
+    if (!fontsLoaded){
+        return <AppLoading/>
+    }
+
+    return(
+        <View 
+            style={{
+                flex: 1, 
+                justifyContent:"center",
+                alignItems:"center"
+            }}
+        >
+            <Text>Wallet App</Text>
+        </View>
+    )
+}
+export default App;
