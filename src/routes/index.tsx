7c6979e9f6b064f67d7de5b5f7f25ce5a42cbbba
Victor Cavalcanti <victorcmarques.dev@gmail.com>
2024-10-09 19:49:52 -0300

    finish project

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { View } from "react-native";
 
 import { AppRoutes } from "./app.routes";
 import { AuthRoutes } from "./auth.routes";
 
 export const Routes = () => {
-    return (
-        <AppRoutes />
-    )
+
+    const user = true;
+
+    return user ? <AppRoutes /> : <AuthRoutes />
+
 }
